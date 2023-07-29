00847736366b658e0891b4e19513bd8e75364798
Youssef Metwally <youssef.mostafa4140@gmail.com>
2023-07-29 05:58:01 +0300

    navbar

--- onlineStore/public/javascript/navbar.js
+++ onlineStore/public/javascript/navbar.js
@@ -1,6 +1,8 @@
 const closeBtn = document.querySelector(".closebtn");
 const dropdownContent = document.querySelector(".dropdown-content-bars");
-const dropbtnbars = document.querySelector(".dropbtn-bars"); 
+const dropbtnbars = document.querySelector(".dropbtn-bars");
+// const dropdownbtn = document.querySelector(".store-drp-btn"); 
+
 
 closeBtn.addEventListener("click", function() {
   dropdownContent.style.display = "none";
@@ -8,6 +10,10 @@ closeBtn.addEventListener("click", function() {
 
 dropbtnbars.addEventListener("click", function() {
   dropdownContent.style.display = "block";
-
-
 });
+
+
+
+
+
+
