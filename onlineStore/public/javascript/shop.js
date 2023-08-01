4614144ab4f542314e0d81cb2056fea2946419ea
Youssef Metwally <youssef.mostafa4140@gmail.com>
2023-08-02 01:03:42 +0300

    styling and cart button

--- onlineStore/public/javascript/shop.js
+++ onlineStore/public/javascript/shop.js
@@ -12,7 +12,7 @@ const pagedown = ()=> {
   changepagenumber()
 }
 
-const pageup = async ()=> {
+const pageup =  ()=> {
   page++
   console.log(page)
   get(`http://localhost:8080/shop?page=${page}`)
@@ -30,20 +30,13 @@ const pageup = async ()=> {
 
   
 
-  const getcount = async (url) => {
-    try {
-      const response = await fetch(url);
-      const result = await response.json();
-      console.log(result)
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+
   const get = async (url) => {
     try {
       const response = await fetch(url);
       const result = await response.json();
       renderproducts(result);
+      console.log(result);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -52,101 +45,125 @@ const pageup = async ()=> {
   get(`http://localhost:8080/shop?page=${page}`);
 
 
-  const renderproducts= (productsdata) => {
-    let productshtml = '';
+  const renderproducts = (productsdata) => {
+    let productshtml = "";
     for (let i = 0; i < productsdata.length; i++) {
       const product = productsdata[i];
       productshtml += `<div class="product-card">
-                 <div class="card-image-container">
-                 <img src="${product.image}">
-             </div>
-                 <div class="product-desc">
-                     <span>${product.title}</span>
-                     <span>${product.price}</span>
-                 </div>
-             <button class="add-cart-btn"><i class="fa-solid fa-cart-plus"></i></button>
-             </div>`
-    } 
-    const productsContainer = document.getElementById('products-container');
+                   <div class="card-image-container">
+                   <img src="${product.image}">
+  
+               </div>
+                   <div class="product-desc">
+                       <span>${product.title}</span>
+                       <span>${product.price}</span>
+                   </div>
+                  <button class="add-cart-btn" onclick="addtocart(this, '${product._id}')"><i class="fa-solid fa-cart-plus"></i></button>
+  
+  
+  
+               </div>`;
+    }
+    const productsContainer = document.getElementById("products-container");
     productsContainer.innerHTML = productshtml;
-  }
+  };
+  
+  const cartlist = [];
+  const addtocart = (button, id) => {
+    cartlist.includes(id) ? cartlist.splice(cartlist.indexOf(id), 1) : cartlist.push(id);
+    button.classList.toggle("added-to-cart");
+    const counter = document.querySelector(".fa-bag-shopping");
+    counter.setAttribute('value', cartlist.length)
+    localStorage.setItem('cartitems', cartlist);
+    console.log(localStorage.getItem('cartitems')); 
+    // console.log(cartlist);
+  };
 
-  // sorting
-  let sort;
 
-  const alphabaticallyAtoZ = document.querySelector("#AtoZ");
-  const alphabaticallyZtoA = document.querySelector("#ZtoA");
-  const priceLowToHigh = document.querySelector("#lowToHigh");
-  const priceHighToLow = document.querySelector("#highToLow");
 
-  alphabaticallyAtoZ.addEventListener("click", function() {
-    sort = 'alphabaticallyAtoZ'
-    get(`http://localhost:8080/shop?page=${page}&sort=${sort}`);
-  });
-  alphabaticallyZtoA .addEventListener("click", function() {
-    sort = 'alphabaticallyZtoA'
-    get(`http://localhost:8080/shop?page=${page}&sort=${sort}`);
+
+class SortAndFilter {
+  constructor(pageUrl, pageNumber) {
+    this.pageUrl = pageUrl;
+    this.pageNumber = pageNumber;
+    this.sort = null;
+    this.alphabaticallyAtoZ = document.querySelector("#AtoZ");
+    this.alphabaticallyZtoA = document.querySelector("#ZtoA");
+    this.priceLowToHigh = document.querySelector("#lowToHigh");
+    this.priceHighToLow = document.querySelector("#highToLow");
+    this.allPriceCheckbox = document.querySelector("#price-all");
+    this.priceCheckboxes = document.querySelectorAll(".pricebox");
+    this.sortbtn = document.querySelector("#sorttext")
+    this.selectedPrices = [];
+
+    this.alphabaticallyAtoZ.addEventListener("click", () => {
+      this.sort = "alphabaticallyAtoZ";
+      this.getSortedResults();
+      this.sortbtn.innerHTML = this.sort;
     });
-  priceLowToHigh.addEventListener("click", function() {
-    sort = 'priceLowToHigh'
-    get(`http://localhost:8080/shop?page=${page}&sort=${sort}`);
-  });
-  priceHighToLow.addEventListener("click", function() {
-    sort = 'priceHighToLow'
-    get(`http://localhost:8080/shop?page=${page}&sort=${sort}`);
-  });
-  
-  
+    this.alphabaticallyZtoA.addEventListener("click", () => {
+      this.sort = "alphabaticallyZtoA";
+      this.getSortedResults();
+      this.sortbtn.innerHTML = this.sort;
+    });
+    this.priceLowToHigh.addEventListener("click", () => {
+      this.sort = "priceLowToHigh";
+      this.getSortedResults();
+      this.sortbtn.innerHTML = this.sort;
 
-
-
-class Filter {
-    constructor() {
-      this.allPriceCheckbox = document.querySelector('#price-all');
-      this.priceCheckboxes = document.querySelectorAll('.pricebox');
-      this.selectedPrices = [];
-  
-      this.priceCheckboxes.forEach((checkbox) => {
-        checkbox.addEventListener('change', () => {
-            if (checkbox.checked) {
-                this.allPriceCheckbox.checked = false;
-            }
-            this.updateSelectedPrices()});
-      });
-  
-      this.allPriceCheckbox.addEventListener('change', () => {
-        if (this.allPriceCheckbox.checked) {
-          this.priceCheckboxes.forEach((checkbox) => {
-            checkbox.checked = false;
-          });
-        }
-        this.updateSelectedPrices()
-      });
-    }
-  
-    updateSelectedPrices() {
-      this.selectedPrices = [];
-  
-      this.priceCheckboxes.forEach((checkbox) => {
+    });
+    this.priceHighToLow.addEventListener("click", () => {
+      this.sort = "priceHighToLow";
+      this.getSortedResults();
+      this.sortbtn.innerHTML = this.sort;
+    });
+    this.priceCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
         if (checkbox.checked) {
-          const priceRange = checkbox.nextElementSibling.textContent;
-          const priceValues = priceRange.split(' - ').map((value) => parseInt(value.replace('$', '')));
-          this.selectedPrices.push(priceValues);
+          this.allPriceCheckbox.checked = false;
         }
+        this.updateSelectedPrices();
       });
-  
-      const flattenPrices = this.selectedPrices.flat();
-      console.log(flattenPrices);
-  
-      const lowestValue = Math.min(...flattenPrices);
-      const highestValue = Math.max(...flattenPrices);
-  
-      console.log("Lowest"+ lowestValue);
-      console.log("Highest" + highestValue);
-      get(`http://localhost:8080/shop?page=${page}&sort=${sort}&range=${lowestValue}X${highestValue}`);
-
-
-    }
+    });
+    this.allPriceCheckbox.addEventListener("change", () => {
+      if (this.allPriceCheckbox.checked) {
+        this.priceCheckboxes.forEach((checkbox) => {
+          checkbox.checked = false;
+        });
+      }
+      this.updateSelectedPrices();
+    });
   }
-  
-  const filter = new Filter();
+
+  getSortedResults() {
+    const url = `${this.pageUrl}?page=${this.pageNumber}&sort=${this.sort}&range=${this.getRange()}`;
+    get(url);
+  }
+
+  updateSelectedPrices() {
+    this.selectedPrices = [];
+
+    this.priceCheckboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        const priceRange = checkbox.value
+        const priceValues = priceRange
+          .split("-")
+        this.selectedPrices.push(priceValues);
+        // console.log(this.selectedPrices)
+
+      }
+    });
+
+    this.getSortedResults();
+  }
+
+  getRange() {
+    const flattenPrices = this.selectedPrices.flat();
+    const lowestValue = Math.min(...flattenPrices);
+    const highestValue = Math.max(...flattenPrices);
+    return `${lowestValue}X${highestValue}`;
+  }
+}
+
+// Example usage:
+const sortAndFilter = new SortAndFilter("http://localhost:8080/shop", page);
