f178d75f6a4b5ea2158df0ea49c97cd7d33f4f03
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-08-02 15:32:44 +0300

    error

--- onlineStore/public/javascript/main.js
+++ onlineStore/public/javascript/main.js
@@ -12,3 +12,39 @@ items.forEach((el) => {
     next = next.nextElementSibling;
   }
 });
+
+// const card = document.getElementById("product-card");
+
+// const getProducts = async () => {
+//   let response = await fetch("https://fakestoreapi.com/products?limit=5");
+//   const data = await response.json();
+//   // console.log(data);
+//   let a = ``;
+
+//   data.forEach((el) => {
+//     a += `
+//     <div class="carousel-item active">
+//     <div class="col-md-3">
+//     <div class="card">
+//                       <div class="image-wrapper">
+//                         <img
+//                           src=${el.image}
+//                           class="card-img-top"
+//                           alt="product image"
+//                           />
+//                           <button class="image-overlay">
+//                           <i class="fa-solid fa-cart-plus"></i>
+//                           </button>
+//                           </div>
+//                           <div class="card-body">
+//                           <h6>${el.title}</h6>
+//                           <p>${el.price}</p>
+//                           </div>
+//                           </div>
+//                           </div>
+//                           </div>
+//                           `;
+//   });
+//   card.innerHTML = a;
+// };
+// getProducts();
